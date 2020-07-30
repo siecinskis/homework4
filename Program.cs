5b18ad982df34598cdbdc74933fa41b8f6681713
siecinskis <68786678+siecinskis@users.noreply.github.com>
2020-07-30 23:42:46 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (; ; )
+            try
+            {
+                
+                {
+                    FizzBuzz fizBuzz = new FizzBuzz();
+                    
+
+                    Console.WriteLine("Witaj użytkowniku - Podaj Liczbę");
+                    var number = int.Parse(Console.ReadLine());
+                    var result = fizBuzz.Game(number);
+                    Console.WriteLine(result);
+
+
+
+                }
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Podałeś błędne dane");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+                
+
+                       
+
+
+        }
+    }
+}
---
+++ fizzBuzz.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework4
+{
+    class FizzBuzz
+    {
+        public string Game(int number)
+        {
+            if ((number % 3 == 0) && (number % 5 == 0))
+            {
+                return "FIZZ BUZZ";
+            }
+            else if (number % 3 == 0)
+            {
+                return "FIZZ";
+            }
+            else if (number % 5 == 0)
+            {
+                return "BUZZ";
+            }
+            else 
+
+            {
+                return number.ToString();
+            }
+            
+        }
+    }
+}
